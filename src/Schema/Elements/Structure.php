41313f30ac2f5935aaeebc8d45c71e517aa52319
Vít Kutný <vit@kutny.cz>
2019-06-19 12:54:13 +0200

    Support older nette/utils

--- src/Schema/Elements/Structure.php
+++ src/Schema/Elements/Structure.php
@@ -142,7 +142,7 @@ final class Structure implements Schema
 			} else {
 				$keys = array_map('strval', array_keys($items));
 				foreach ($extraKeys as $key) {
-					$hint = Nette\Utils\Helpers::getSuggestion($keys, (string) $key);
+					$hint = self::getSuggestion($keys, (string) $key);
 					$context->addError(
 						'Unexpected option %path%' . ($hint ? ", did you mean '%hint%'?" : '.'),
 						Nette\Schema\ValidationException::UNEXPECTED_STRUCTURE_KEY,
@@ -179,5 +179,29 @@ final class Structure implements Schema
 	public function completeDefault(Context $context)
 	{
 		return $this->complete([], $context);
+	}
+
+
+	/**
+	 * Finds the best suggestion (for 8-bit encoding).
+	 * @param  (\ReflectionFunctionAbstract|\ReflectionParameter|\ReflectionClass|\ReflectionProperty|string)[]  $possibilities
+	 * @internal
+	 */
+	private static function getSuggestion(array $possibilities, string $value): ?string
+	{
+		$norm = preg_replace($re = '#^(get|set|has|is|add)(?=[A-Z])#', '', $value);
+		$best = null;
+		$min = (strlen($value) / 4 + 1) * 10 + .1;
+		foreach (array_unique($possibilities, SORT_REGULAR) as $item) {
+			$item = $item instanceof \Reflector ? $item->getName() : $item;
+			if ($item !== $value && (
+					($len = levenshtein($item, $value, 10, 11, 10)) < $min
+					|| ($len = levenshtein(preg_replace($re, '', $item), $norm, 10, 11, 10) + 20) < $min
+				)) {
+				$min = $len;
+				$best = $item;
+			}
+		}
+		return $best;
 	}
 }
